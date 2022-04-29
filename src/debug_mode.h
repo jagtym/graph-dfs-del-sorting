021674d6a7f27e75449ce49394e0bcaa178ef11a
jagtym <jagtymxd@gmail.com>
2022-04-29 16:41:08 +0200

    Remove matrix prints

--- src/debug_mode.h
+++ src/debug_mode.h
@@ -1,14 +1,12 @@
 void debug_mode(bool file)
 {
-    std::cout << "Debug mode running." << std::endl;
-
     GraphMatrix matrix = GraphMatrix(file);
     
-    matrix.printEdges();
-    matrix.printMatrix();
-    matrix.printNexts();
-    matrix.printPrevs();
-    matrix.printNonInc();
+    // matrix.printEdges();
+    // matrix.printMatrix();
+    // matrix.printNexts();
+    // matrix.printPrevs();
+    // matrix.printNonInc();
 
     matrix.sort_DFS();
     matrix.sort_DEL();
